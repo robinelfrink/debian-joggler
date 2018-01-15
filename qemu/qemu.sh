0bc54ddfe29d56ddd35168a0382842883bb32daf
Robin Elfrink <robin.elfrink@eu.equinix.com>
2018-01-15 14:58:01 +0100

    Have the nic be called `ens1` in qemu too.
    Also, add a helper script to run the qemu emulator.

---
+++ qemu/qemu.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SCRIPT=$0
+ROOT=`readlink -f $(dirname $(readlink -f ${SCRIPT}))/../`
+
+qemu-system-i386 -bios qemu/bios32.bin \
+    -machine pc -cpu n270 \
+    -m 512M -usb -k en-us -monitor stdio \
+    -device VGA \
+    -device pci-bridge,id=pci_bridge1,chassis_nr=1 \
+    -device rtl8139,netdev=nic1,bus=pci_bridge1,addr=1 \
+    -device usb-storage,drive=internal \
+    -device usb-storage,drive=external \
+    -drive if=none,format=raw,id=internal,file=${ROOT}/qemu/internal.img \
+    -drive if=none,format=raw,id=external,file=${ROOT}/joggler.img \
+    -netdev user,id=nic1,net=10.255.0.0/24,dhcpstart=10.255.0.10,hostfwd=tcp::1222-:22
