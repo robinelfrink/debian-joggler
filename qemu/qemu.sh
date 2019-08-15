3806babcd9d042db496479f1225416a43fc71ec9
Robin Elfrink <robin@15augustus.nl>
2019-08-15 09:27:35 +0200

    Emulate a tablet/touchscreen instead of mouse.

--- qemu/qemu.sh
+++ qemu/qemu.sh
@@ -11,6 +11,7 @@ qemu-system-i386 -bios qemu/bios32.bin \
     -device rtl8139,netdev=nic1,bus=pci_bridge1,addr=1 \
     -device usb-storage,drive=internal \
     -device usb-storage,drive=external \
+    -usbdevice tablet \
     -drive if=none,format=raw,id=internal,file=${ROOT}/qemu/internal.img \
     -drive if=none,format=raw,id=external,file=${ROOT}/joggler.img \
     -netdev user,id=nic1,net=10.255.0.0/24,dhcpstart=10.255.0.10,hostfwd=tcp::1222-:22
