6365525f5822dbfd029027c798caf4a8e9b98fe8
Robin Elfrink <robin@15augustus.nl>
2020-04-13 20:31:15 +0200

    Small qemu changes.

--- qemu/qemu.sh
+++ qemu/qemu.sh
@@ -5,14 +5,15 @@ ROOT=$(readlink -f $(dirname $(readlink -f ${SCRIPT}))/../)
 
 qemu-system-i386 -bios qemu/bios32.bin \
     -machine pc -cpu n270 \
+    -enable-kvm -accel kvm \
     -m 512M -usb -k en-us -monitor stdio \
     -vga none \
-    -device virtio-vga,xres=800,yres=480 \
+    -device qxl-vga,xres=800,yres=480 \
     -device pci-bridge,id=pci_bridge1,chassis_nr=1 \
     -device rtl8139,netdev=nic1,bus=pci_bridge1,addr=1 \
     -device usb-storage,drive=internal \
     -device usb-storage,drive=external \
-    -device usb-tablet \
+    -device usb-wacom-tablet \
     -drive if=none,format=raw,id=internal,file=${ROOT}/qemu/internal.img \
     -drive if=none,format=raw,id=external,file=${ROOT}/joggler.img \
     -netdev user,id=nic1,net=10.255.0.0/24,dhcpstart=10.255.0.10,hostfwd=tcp::1222-:22
